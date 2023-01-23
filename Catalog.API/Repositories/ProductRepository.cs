8ff4c87cfabaf77bee24db56300f990c14a728dc
mayconbj15 <mayconbj15@gmail.com>
2023-01-22 22:37:11 -0300

    Create repositories

---
+++ Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,59 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task CreateProductAsync(Product product)
+        {
+            await _context.Products.InsertOneAsync(product);  
+        }
+
+        public async Task<bool> DeleteProductAsync(string id)
+        {
+            var deleteResult = await _context.Products.DeleteOneAsync(id);
+
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+        }
+
+        public async Task<Product> GetProductByIdAsync(string id)
+        {
+            return await _context.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await _context.Products.Find(p => true).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByCategoryAsync(string categoryName)
+        {
+            var filter = Builders<Product>.Filter.Eq(p => p.Category, categoryName);
+
+            return await _context.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByNameAsync(string name)
+        {
+            var filter = Builders<Product>.Filter.Eq(p => p.Name, name);
+
+            return await _context.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task<bool> UpdateProductAsync(Product product)
+        {
+            var updateResult = await _context.Products.ReplaceOneAsync(p => p.Id == product.Id, product);
+
+            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
+        }
+    }
+}
