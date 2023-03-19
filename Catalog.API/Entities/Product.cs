8d892b9d8ab2a5a55770d1c609743200b2eee42f
mayconbj15 <mayconbj15@gmail.com>
2023-03-19 19:49:23 -0300

    Add mongo seed container

--- Catalog.API/Entities/Product.cs
+++ Catalog.API/Entities/Product.cs
@@ -6,12 +6,17 @@ namespace Catalog.API.Entities
     {
         [BsonId]
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        [BsonElement("_id")]
         public string Id { get; set; }
-
+        [BsonElement("name")]
         public string Name { get; set; }
+        [BsonElement("category")]
         public string Category { get; set; }
+        [BsonElement("description")]
         public string Description { get; set; }
+        [BsonElement("image")]
         public string Image { get; set; }
+        [BsonElement("price")]
         public decimal Price { get; set; }
     }
 }
