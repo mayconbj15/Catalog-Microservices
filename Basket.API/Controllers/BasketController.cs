0532c84a28b99bc7035f9338a97394100b6754c3
mayconbj15 <mayconbj15@gmail.com>
2023-03-21 22:18:48 -0300

    Add basket repository and controller

---
+++ Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,40 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers
+{
+    [Route("/api/v1/[controller]")]
+    [ApiController]
+    public class BasketController : Controller
+    {
+        private readonly IBasketRepository _repository;
+
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("username")]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string username)
+        {
+            var cart = await _repository.GetBasketAsync(username);
+
+            return Ok(cart ?? new ShoppingCart(username));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart cart)
+        {
+            return Ok(await _repository.UpdateBasketAsync(cart));
+        }
+
+        [HttpDelete("username")]
+        public async Task<IActionResult> DeleteBasket(string username)
+        {
+            await _repository.DeleteBasketAsync(username);
+
+            return StatusCode(204);
+        }
+    }
+}
---
+++ Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,38 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _cache;
+
+        public BasketRepository(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public async Task DeleteBasketAsync(string username)
+        {
+            await _cache.RemoveAsync(username);
+        }
+
+        public async Task<ShoppingCart> GetBasketAsync(string username)
+        {
+            var json = await _cache.GetStringAsync(username);
+
+            if(string.IsNullOrWhiteSpace(json)) 
+                return default;
+
+            return JsonSerializer.Deserialize<ShoppingCart>(json);
+        }
+
+        public async Task<ShoppingCart> UpdateBasketAsync(ShoppingCart cart)
+        {
+            await _cache.SetStringAsync(cart.Username, JsonSerializer.Serialize(cart));
+
+            return cart;
+        }
+    }
+}
