457d7f3607e50d2ad2a414fb6f1ed0e864ea6249
mayconbj15 <mayconbj15@gmail.com>
2023-05-16 21:57:16 -0300

    Create Discount.Grpc and define contracts

--- Discount.API/Program.cs
+++ Discount.API/Program.cs
@@ -12,11 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IDbConnection, NpgsqlConnection>(prov =>
-{
-    var connectionString = builder.Configuration.GetValue<string>("DatabaseSettings:ConnectionString");
-    return new NpgsqlConnection(connectionString);
-});
+builder.Services.AddScoped<IDbConnection, NpgsqlConnection>(prov => new NpgsqlConnection(builder.Configuration.GetValue<string>("DatabaseSettings:ConnectionString")));
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 var app = builder.Build();
